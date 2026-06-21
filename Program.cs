2835a214ec1412ce27e226111301b8c378ad5bd8
Sumeet Shah <sumeetshah7007@gmail.com>
2026-06-21 12:00:00 +0530

    Update Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using AIPRReviewer;
+using DotNetEnv;
+
+Env.Load();
+
+int? prNumber = args.Length > 0 ? int.Parse(args[0]) : null;
+
+var orchestrator = new Orchestrator();
+await orchestrator.RunAsync(prNumber);
